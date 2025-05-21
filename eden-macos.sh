814a1662b5ab3f71351ba9977b56720538fe597a
Bernardo Arocho <hauntek@hotmail.com>
2025-05-21 18:31:58 +0800

    FixMachOLibraryPaths (#15)
    Fixes dynamic library paths in macOS application bundles by:
    1. Changing @rpath references to use @executable_path
    2. Updating dylib IDs to use proper relative paths

--- eden-macos.sh
+++ eden-macos.sh
@@ -46,6 +46,23 @@ ccache -s -v
 APP=./bin/eden.app
 macdeployqt "$APP"
 macdeployqt "$APP" -always-overwrite -verbose=2
+
+# FixMachOLibraryPaths
+find "$APP/Contents/Frameworks" ""$APP/Contents/MacOS"" -type f \( -name "*.dylib" -o -perm +111 \) | while read file; do
+    if file "$file" | grep -q "Mach-O"; then
+        otool -L "$file" | awk '/@rpath\// {print $1}' | while read lib; do
+            lib_name="${lib##*/}"
+            new_path="@executable_path/../Frameworks/$lib_name"
+            install_name_tool -change "$lib" "$new_path" "$file"
+        done
+
+        if [[ "$file" == *.dylib ]]; then
+            lib_name="${file##*/}"
+            new_id="@executable_path/../Frameworks/$lib_name"
+            install_name_tool -id "$new_id" "$file"
+        fi
+    fi
+done
 codesign --deep --force --verify --verbose --sign - "$APP"
 
 # Pack for upload
