abe462afcd0e02a15b97b1e7ab917ff83616b30d
Escary <81011361+pflyly@users.noreply.github.com>
2025-06-21 22:11:52 +0100

    lose macos workaround (#52)

--- eden-macos.sh
+++ eden-macos.sh
@@ -3,39 +3,12 @@
 echo "Making Eden for MacOS"
 if [ "$TARGET" = "arm64" ]; then
     export LIBVULKAN_PATH="/opt/homebrew/lib/libvulkan.1.dylib"
-    INCLUDE_DIR="/opt/homebrew/include/libavcodec"
 else
     export LIBVULKAN_PATH="/usr/local/lib/libvulkan.1.dylib"
-    INCLUDE_DIR="/usr/local/include/libavcodec"
 fi
 
-# Workaround for ffmpeg
-git clone --depth=1 https://github.com/FFmpeg/FFmpeg.git ffmpeg
-cd ffmpeg
-./configure \
-    --disable-avdevice \
-    --arch=$TARGET \
-    --disable-avformat \
-    --disable-doc \
-    --disable-everything \
-    --disable-ffmpeg \
-    --disable-ffprobe \
-    --disable-network \
-    --disable-swresample \
-    --disable-vaapi \
-    --disable-vdpau \
-    --enable-decoder=h264 \
-    --enable-decoder=vp8 \
-    --enable-decoder=vp9 \
-    --enable-avfilter \
-    --enable-shared \
-    --disable-iconv \
-    --enable-filter=yadif,scale
-cp -v ./libavcodec/codec_internal.h ./config.h "$INCLUDE_DIR/"
-
-cd ..
 # Clone Eden, fallback to mirror if upstream repo fails to clone
-if ! git clone 'https://git.eden-emu.dev/eden-emu/eden.git' ./eden; then
+if ! git clone https://git.eden-emu.dev/eden-emu/eden.git ./eden; then
 	echo "Using mirror instead..."
 	rm -rf ./eden || true
 	git clone 'https://github.com/pflyly/eden-mirror.git' ./eden
@@ -55,8 +28,6 @@ cmake .. -GNinja \
     -DENABLE_QT_TRANSLATION=ON \
     -DYUZU_ENABLE_LTO=ON \
     -DUSE_DISCORD_PRESENCE=OFF \
-    -DUSE_SYSTEM_MOLTENVK=ON \
-    -DYUZU_USE_BUNDLED_FFMPEG=OFF \
     -DYUZU_CMD=OFF \
     -DYUZU_ROOM_STANDALONE=OFF \
     -DCMAKE_OSX_ARCHITECTURES="$TARGET" \
@@ -72,7 +43,7 @@ ccache -s -v
 # Bundle and code-sign eden.app. Excute twice to resolve the qt framework missing error, thanks to @hauntek!
 APP=./bin/eden.app
 macdeployqt "$APP"
-macdeployqt "$APP" -always-overwrite -verbose=2
+macdeployqt "$APP" -always-overwrite
 
 # FixMachOLibraryPaths
 find "$APP/Contents/Frameworks" ""$APP/Contents/MacOS"" -type f \( -name "*.dylib" -o -perm +111 \) | while read file; do
@@ -95,7 +66,7 @@ codesign --deep --force --verify --verbose --sign - "$APP"
 # Pack for upload
 mkdir -p artifacts
 mkdir "$APP_NAME"
-cp -av ./bin/. "$APP_NAME"
+cp -a ./bin/. "$APP_NAME"
 ZIP_NAME="$APP_NAME.7z"
 7z a -t7z -mx=9 "$ZIP_NAME" "$APP_NAME"
 mv "$ZIP_NAME" artifacts/
