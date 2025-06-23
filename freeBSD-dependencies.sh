8b4acf15c1ef929ab1f432d4c5f4e45b5049550b
Escary <81011361+pflyly@users.noreply.github.com>
2025-06-23 10:18:31 +0100

    Revert "Use ffmpeg workaround for freebsd (#55)" (#56)
    This reverts commit 28e1cfb6ceac5501e3dda64047b7ea8faecf923f.

--- freeBSD-dependencies.sh
+++ freeBSD-dependencies.sh
@@ -5,6 +5,6 @@ sed -i '' -e 's/quarterly/latest/' /etc/pkg/FreeBSD.conf
 export ASSUME_ALWAYS_YES=true
 
 pkg install autoconf bash boost-libs catch2 cmake ccache enet ffmpeg fusefs-libs \
-            gcc gmake git glslang llvm libfmt libzip liblz4 lzlib mbedtls3 nasm \
+            gcc git glslang llvm libfmt libzip liblz4 lzlib mbedtls3 nasm \
             ninja nlohmann-json openssl opus pkgconf qt6-base qt6ct qt6-multimedia \
             qt6-tools qt6-translations qt6-wayland sdl2 sdl3 unzip vulkan-headers wget zip zstd
